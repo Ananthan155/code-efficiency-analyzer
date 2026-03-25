cada15dbccc16c86b09c170eeb2e5e5d28e9e338
Ananthapadmanabhan K <brindavanasaaranga.2@gmail.com>
2026-03-25 18:37:36 +0530

    Update main execution block to use port 5000

--- backend/app.py
+++ backend/app.py
@@ -142,6 +142,5 @@ def train_model():
 
 import os
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
